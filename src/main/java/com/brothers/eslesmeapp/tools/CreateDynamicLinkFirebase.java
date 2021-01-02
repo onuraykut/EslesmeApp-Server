f776732d618df18df55b80c1703dfff33ba7fab7
Onur Aykut <onuraykut@outlook.com>
2021-01-02 14:27:46 +0300

    update dynamic links

--- src/main/java/com/brothers/eslesmeapp/tools/CreateDynamicLinkFirebase.java
+++ src/main/java/com/brothers/eslesmeapp/tools/CreateDynamicLinkFirebase.java
@@ -8,8 +8,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
 
 public class CreateDynamicLinkFirebase {
-    private final String FIREBASE_SERVER_KEY = "AAAAUUps8vw:APA91bFIfywanUfQTWtApzHfaGy23ZgRhz4E9hWqRWXs0gHZgRDfboDpHrbAsDw_KELHgduj4MDJOS_8ipj5m8rDduwB0mjIEBHCApEaEwPzUo9o2GMgqTFbzDQyAhse9SwRhkrMHXpB";
-    private final String FIREBASE_WEB_API_KEY = " AIzaSyCEZPe-puDMZ_VadHdND-LuoqB5mMwRVnE";
+    private final String FIREBASE_SERVER_KEY = "AAAAZvToyvc:APA91bE0xjK9ubzPdrzrqjzLW2uCg2BZZCqiq3c0_wOVfO8wbj3_PJ77UJSogiZYeLJ7qHNIefvA1uw6hdUS8Qks0hsHrZn4ZZA1ND-QEqSym9KnXn9r07Y-oeYFf6GdltozENJi0lBC";
+    private final String FIREBASE_WEB_API_KEY = "AIzaSyBdgIXvIlTz0YwJESxaAYhaA_oAbndddVI";
     private final String FIREBASE_API_URL = "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key="+FIREBASE_WEB_API_KEY;
 
 
@@ -22,7 +22,7 @@ public class CreateDynamicLinkFirebase {
         msg.put("domainUriPrefix", "https://eslesmeapp.page.link");
         msg.put("link", "https://ucretsizkitapindir.com/"+testId+"-"+roomId);
         JSONObject android = new JSONObject();
-        android.put("androidPackageName", "com.brothers.eslesmeapp");
+        android.put("androidPackageName", "com.brothersoftware.eslesmeapp");
         msg.put("androidInfo", android);
        
 
